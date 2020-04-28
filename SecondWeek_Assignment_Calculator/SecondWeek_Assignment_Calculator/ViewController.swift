1396d2bdb8c3348501ed4f40bd5f91e65fd83b80
jaeukyang <didwodnr123@gmail.com>
2020-04-28 19:37:19 +0900

    1차 과제 폴더 이름 변경

--- SecondWeek_Assignment_Calculator/SecondWeek_Assignment_Calculator/ViewController.swift
+++ SecondWeek_Assignment_Calculator/SecondWeek_Assignment_Calculator/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var btnOne: UIButton!
+    @IBOutlet var btnTwo: UIButton!
+    @IBOutlet var btnThree: UIButton!
+    @IBOutlet var btnDev: UIButton!
+    @IBOutlet var btnFour: UIButton!
+    @IBOutlet var btnFive: UIButton!
+    @IBOutlet var btnSix: UIButton!
+    @IBOutlet var btnMul: UIButton!
+    @IBOutlet var btnSeven: UIButton!
+    @IBOutlet var btnEight: UIButton!
+    @IBOutlet var btnNine: UIButton!
+    @IBOutlet var btnSub: UIButton!
+    @IBOutlet var btnAC: UIButton!
+    @IBOutlet var btnZero: UIButton!
+    @IBOutlet var btnResult: UIButton!
+    @IBOutlet var btnPlus: UIButton!
+    
+   
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        btnOne.layer.cornerRadius = 35
+        btnTwo.layer.cornerRadius = 35
+        btnThree.layer.cornerRadius = 35
+        btnDev.layer.cornerRadius = 35
+        btnFour.layer.cornerRadius = 35
+        btnFive.layer.cornerRadius = 35
+        btnSix.layer.cornerRadius = 35
+        btnMul.layer.cornerRadius = 35
+        btnSeven.layer.cornerRadius = 35
+        btnEight.layer.cornerRadius = 35
+        btnNine.layer.cornerRadius = 35
+        btnSub.layer.cornerRadius = 35
+        btnAC.layer.cornerRadius = 35
+        btnZero.layer.cornerRadius = 35
+        btnResult.layer.cornerRadius = 35
+        btnPlus.layer.cornerRadius = 35
+        
     }
 
 
