13eb2e2443326137bb7e2e1c5fe674c0eabe2050
jaeukyang <didwodnr123@gmail.com>
2020-04-28 19:20:34 +0900

    2차 과제 계산기 UI

---
+++ SecondWeek_Assignment_Calculator/SecondWeek_Assignment_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondWeek_Assignment_Calculator
+//
+//  Created by 양재욱 on 2020/04/28.
+//  Copyright © 2020 양재욱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
